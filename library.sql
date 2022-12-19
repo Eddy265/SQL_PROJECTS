0d5c4370276a10f203958ddebc7b0879b4552a28
Edwin Uzoefuna <edwinuzoefuna@gmail.com>
2022-12-19 16:36:32 +0100

    table and insertion done

--- library.sql
+++ library.sql
@@ -22,7 +22,47 @@ CREATE TABLE tbl_library_branch (
 		library_branch_BranchAddress VARCHAR(200) NOT NULL
 	);
 
+CREATE TABLE tbl_borrower (
+		borrower_CardNo INT PRIMARY KEY NOT NULL,
+		borrower_BorrowerName VARCHAR(100) NOT NULL,
+		borrower_BorrowerAddress VARCHAR(200) NOT NULL,
+		borrower_BorrowerPhone VARCHAR(50) NOT NULL
+	);
 
+
+SELECT * FROM tbl_borrower
+
+
+CREATE TABLE tbl_book_loans (
+	book_loans_LoansID serial PRIMARY KEY NOT NULL,
+	book_loans_BookID INT NOT NULL,
+	book_loans_BranchID INT NOT NULL,
+	book_loans_CardNo INT NOT NULL,
+	book_loans_DateOut VARCHAR(50) NOT NULL,
+	book_loans_DueDate VARCHAR(50) NOT NULL,
+	FOREIGN KEY (book_loans_BookID) REFERENCES tbl_book(book_BookID) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (book_loans_BranchID) REFERENCES tbl_library_branch(library_branch_BranchID) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (book_loans_CardNo) REFERENCES tbl_borrower(borrower_CardNo) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE TABLE tbl_book_copies (
+	book_copies_CopiesID serial PRIMARY KEY NOT NULL,
+	book_copies_BookID INT NOT NULL,
+	book_copies_BranchID INT NOT NULL,
+	book_copies_No_Of_Copies INT NOT NULL,
+	FOREIGN KEY (book_copies_BookID) REFERENCES tbl_book(book_BookID) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (book_copies_BranchID) REFERENCES tbl_library_branch(library_branch_BranchID) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+SELECT * FROM tbl_book_copies
+
+CREATE TABLE tbl_book_authors (
+	book_authors_AuthorID serial PRIMARY KEY NOT NULL,
+	book_authors_BookID INT NOT NULL,
+	book_authors_AuthorName VARCHAR(50) NOT NULL,
+	FOREIGN KEY (book_authors_BookID) REFERENCES tbl_book(book_BookID) ON UPDATE CASCADE ON DELETE CASCADE);
+
+
+SELECT * FROM tbl_book_authors
 
 /*INSERT INTO TABLES*/
 
@@ -79,3 +119,160 @@ INSERT INTO tbl_library_branch
 		(2,'Central','491 3rd Street, New York, NY 10014'),
 		(3,'Saline','40 State Street, Saline, MI 48176'),
 		(4,'Ann Arbor','101 South University, Ann Arbor, MI 48104');
+
+
+
+INSERT INTO tbl_borrower
+		(borrower_CardNo, borrower_BorrowerName, borrower_BorrowerAddress, borrower_BorrowerPhone)
+		VALUES
+		(1,'Joe Smith','1321 4th Street, New York, NY 10014','212-312-1234'),
+		(2,'Jane Smith','1321 4th Street, New York, NY 10014','212-931-4124'),
+		(3,'Tom Li','981 Main Street, Ann Arbor, MI 48104','734-902-7455'),
+		(4,'Angela Thompson','2212 Green Avenue, Ann Arbor, MI 48104','313-591-2122'),
+		(5,'Harry Emnace','121 Park Drive, Ann Arbor, MI 48104','412-512-5522'),
+		(6,'Tom Haverford','23 75th Street, New York, NY 10014','212-631-3418'),
+		(7,'Haley Jackson','231 52nd Avenue New York, NY 10014','212-419-9935'),
+		(8,'Michael Horford','653 Glen Avenue, Ann Arbor, MI 48104','734-998-1513');
+
+        
+        
+INSERT INTO tbl_book_loans
+	(book_loans_BookID, book_loans_BranchID, book_loans_CardNo, book_loans_DateOut, book_loans_DueDate) 
+	VALUES
+		('1','4','2','1/12/18','2/12/18'),
+		('3','4','7','1/3/18','2/3/18'),
+		('18','4','7','1/3/18','2/3/18'),
+		('12','4','2','1/4/18','2/4/18'),
+		('11','4','3','1/15/18','2/15/18'),
+		('9','4','3','1/15/18','2/15/18'),
+		('7','4','7','1/1/18','2/2/18'),
+		('4','4','3','1/1/18','2/2/18'),
+		('1','4','3','2/2/17','3/2/18'),
+		('20','4','3','1/3/18','2/3/18'),
+		('1','4','2','1/12/18','2/12/18'),
+		('3','4','7','1/13/18','2/13/18'),
+		('18','4','7','1/13/18','2/13/18'),
+		('12','4','2','1/14/18','2/14/18'),
+		('11','4','3','1/15/18','2/15/18'),
+		('9','4','3','1/15/18','2/15/18'),
+		('7','4','7','1/19/18','2/19/18'),
+		('4','4','3','1/19/18','2/19/18'),
+		('1','4','3','1/22/18','2/22/18') RETURNING book_loans_LoansID;
+
+		
+	SELECT * FROM tbl_book_loans
+
+INSERT INTO tbl_book_copies
+		(book_copies_BookID, book_copies_BranchID, book_copies_No_Of_Copies)
+		VALUES
+		('1','1','5'),
+		('2','1','5'),
+		('3','1','5'),
+		('4','1','5'),
+		('5','1','5'),
+		('6','1','5'),
+		('7','1','5'),
+		('8','1','5'),
+		('9','1','5'),
+		('10','1','5'),
+		('11','1','5'),
+		('12','1','5'),
+		('13','1','5'),
+		('14','1','5'),
+		('15','1','5'),
+		('16','1','5'),
+		('17','1','5'),
+		('18','1','5'),
+		('19','1','5'),
+		('20','1','5'),
+		('1','2','5'),
+		('2','2','5'),
+		('3','2','5'),
+		('4','2','5'),
+		('5','2','5'),
+		('6','2','5'),
+		('7','2','5'),
+		('8','2','5'),
+		('9','2','5'),
+		('10','2','5'),
+		('11','2','5'),
+		('12','2','5'),
+		('13','2','5'),
+		('14','2','5'),
+		('15','2','5'),
+		('16','2','5'),
+		('17','2','5'),
+		('18','2','5'),
+		('19','2','5'),
+		('20','2','5'),
+		('1','3','5'),
+		('2','3','5'),
+		('3','3','5'),
+		('4','3','5'),
+		('5','3','5'),
+		('6','3','5'),
+		('7','3','5'),
+		('8','3','5'),
+		('9','3','5'),
+		('10','3','5'),
+		('11','3','5'),
+		('12','3','5'),
+		('13','3','5'),
+		('14','3','5'),
+		('15','3','5'),
+		('16','3','5'),
+		('17','3','5'),
+		('18','3','5'),
+		('19','3','5'),
+		('20','3','5'),
+		('1','4','5'),
+		('2','4','5'),
+		('3','4','5'),
+		('4','4','5'),
+		('5','4','5'),
+		('6','4','5'),
+		('7','4','5'),
+		('8','4','5'),
+		('9','4','5'),
+		('10','4','5'),
+		('11','4','5'),
+		('12','4','5'),
+		('13','4','5'),
+		('14','4','5'),
+		('15','4','5'),
+		('16','4','5'),
+		('17','4','5'),
+		('18','4','5'),
+		('19','4','5'),
+		('20','4','5');
+
+	SELECT * FROM tbl_book_copies
+ 
+
+INSERT INTO tbl_book_authors
+		(book_authors_BookID,book_authors_AuthorName)
+		VALUES
+		('1','Patrick Rothfuss'),
+		('2','Stephen King'),
+		('3','Stephen King'),
+		('4','Frank Herbert'),
+		('5','J.R.R. Tolkien'),
+		('6','Christopher Paolini'),
+		('6','Patrick Rothfuss'),
+		('8','J.K. Rowling'),
+		('9','Haruki Murakami'),
+		('10','Shel Silverstein'),
+		('11','Douglas Adams'),
+		('12','Aldous Huxley'),
+		('13','William Goldman'),
+		('14','Chuck Palahniuk'),
+		('15','Louis Sachar'),
+		('16','J.K. Rowling'),
+		('17','J.K. Rowling'),
+		('18','J.R.R. Tolkien'),
+		('19','George R.R. Martin'),
+		('20','Mark Lee');
+
+
+/*Exercises*/
+1
