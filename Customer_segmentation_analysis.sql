f485ce12f3dcd0c2729ac6b3bc3921cc36256fdc
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-01-21 04:03:18 +0100

    Customer Segmentation Analysis

--- Customer_segmentation_analysis.sql
+++ Customer_segmentation_analysis.sql
@@ -3,8 +3,8 @@ such as demographics, behavior, and purchase history. The goal of customer segme
 high yield segments - that is, those segments that are likely to be the most profitable or that have 
 growth potential - and to develop strategies to target these segments.
 
-In this project, I used SQL to extract and analyze data from the customer table. The table contains 
-information about customers, such as their user_id, gender, age, salary, and purchase history (1 or 0).
+In this project, I used SQL to extract and analyze data from the customer_seg table. The table contains 
+information about customers, such as their user_id, gender, age, salary, and purchase history (1 or 0) and date.
 
 1. What is the breakdown of customers by gender? */
 SELECT "gender", COUNT(DISTINCT "user_id") as num_customers
