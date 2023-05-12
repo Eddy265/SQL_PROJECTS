b1aaa2d62a02d7a3bc553d65d373f5be1350cf61
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-05-12 16:01:28 +0200

    Customer loyalty and satisfaction

--- NPS_ANALYSIS_INSTACART_DB.sql
+++ NPS_ANALYSIS_INSTACART_DB.sql
@@ -1,19 +1,24 @@
-/*Project Title: Customer Satisfaction Analysis for Least Performing Products:
+/*Project Title: Customer Satisfaction and Loyalty Analysis
 
 Project Description:
 
-This project aimed to identify the 10 least performing products in terms of quantity ordered and analyze customer satisfaction for these products using the Net Promoter Score (NPS) framework.
+This project aimed to identify the 10 least performing products in terms of quantity ordered and analyze customer satisfaction for these products using the 
+Net Promoter Score (NPS) framework.
+First, I analyzed the sales data of the products from the Instacart database and identified the 10 products that had the lowest quantity of orders. I then prepared a 
+customer survey with a question asking customers to rate their likelihood of recommending these products to a friend or colleague on a scale of 0-10.
+I distributed the survey to a sample of customers from different segments and collected their responses. The survey form was created using google form that was 
+connected to the database. Using the NPS framework, I calculated the percentage of promoters (customers who rated the product 9 or 10), detractors (customers who 
+rated the product 0 to 6), and passives (customers who rated the product 7 or 8) for each of the 10 least performing products.
+My analysis showed that the NPS scores for the 10 products ranged from -60% to 35.71%, indicating that most of the customers were dissatisfied with these products. 
+I also found that the percentage of passive respondents for each product varied between 25% to 50%, indicating that these customers are not completely satisfied with 
+the product and may switch to other products in the future.
 
-First, I analyzed the sales data of the products from the Instacart database and identified the 10 products that had the lowest quantity of orders. I then prepared a customer survey with a question asking customers to rate their likelihood of recommending these products to a friend or colleague on a scale of 0-10.
+Based on these findings, I recommended that the company take appropriate actions to improve the quality of these products and also incentivize customers to continue
+purchasing them. For example, the company can consider providing discounts or loyalty points to customers who continue to purchase these products despite their low 
+satisfaction levels.
 
-I distributed the survey to a sample of customers from different segments and collected their responses. The survey form was created using google form that was connected to the database. Using the NPS framework, I calculated the percentage of promoters (customers who rated the product 9 or 10), detractors (customers who rated the product 0 to 6), and passives (customers who rated the product 7 or 8) for each of the 10 least performing products.
-
-My analysis showed that the NPS scores for the 10 products ranged from -60% to 35.71%, indicating that most of the customers were dissatisfied with these products. I also found that the percentage of passive respondents for each product varied between 25% to 50%, indicating that these customers are not completely satisfied with the product and may switch to other products in the future.
-
-
-Based on these findings, I recommended that the company take appropriate actions to improve the quality of these products and also incentivize customers to continue purchasing them. For example, the company can consider providing discounts or loyalty points to customers who continue to purchase these products despite their low satisfaction levels.
-
-Overall, this project helped the company identify the least performing products and understand the reasons behind the low customer satisfaction levels. The NPS framework provided a quantitative measure of customer satisfaction and helped in prioritizing the products that require immediate attention.*/
+Overall, this project helped the company identify the least performing products and understand the reasons behind the low customer satisfaction levels. The NPS 
+framework provided a quantitative measure of customer satisfaction and helped in prioritizing the products that require immediate attention.*/
 
 
 
