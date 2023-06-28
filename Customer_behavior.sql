2cad2006b34af53a24bbca2631f2de8a7668dec1
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-06-28 11:34:25 +0200

    Added more business questions

--- Customer_behavior.sql
+++ Customer_behavior.sql
@@ -1,10 +1,18 @@
 
 /* Comparative Analysis of Customer Purchase Patterns: Unveiling Customer Behavior Insights
-Understanding customer behavior is crucial for businesses to make informed decisions and devise effective strategies. One valuable aspect of customer behavior analysis is comparing the purchase patterns over time. By examining the purchase pattern, businesses can gain valuable insights into customer trends and behaviors.
-This project focuses on conducting a comprehensive analysis of customer purchase patterns and fluctuations in customer behavior. These insights can help businesses tailor their marketing efforts, optimize their retention strategies, and enhance customer satisfaction.
-In addition, It examines customer retention rates, which allows businesses to detect any seasonal trends in customer retention rates, enabling them to plan and allocate resources accordingly.
-Furthermore, the project delves into age-based purchasing patterns, examining which age brackets make the most and least purchases. This analysis enables businesses to identify specific age groups that exhibit higher or lower purchase behavior, facilitating targeted marketing and sales efforts. By understanding the purchasing preferences of different age groups, businesses can tailor their strategies and offer to effectively engage and retain customers.
-Finally, the project investigates the average purchase value of customers within cohorts, focusing specifically on the year 2021. This analysis helps businesses comprehend how customer behavior and purchase habits evolve over time within different cohorts. By identifying trends and changes in average purchase values, businesses can make data-driven decisions to improve their marketing strategies, optimize customer engagement, and enhance overall customer lifetime value.
+Understanding customer behavior is crucial for businesses to make informed decisions and devise effective strategies. One valuable aspect of customer 
+behavior analysis is comparing the purchase patterns over time. By examining the purchase pattern, businesses can gain valuable insights into customer 
+trends and behaviors.
+This project focuses on conducting a comprehensive analysis of customer purchase patterns and fluctuations in customer behavior. These insights can help
+businesses tailor their marketing efforts, optimize their retention strategies, and enhance customer satisfaction.
+In addition, It examines customer retention rates, which allows businesses to detect any seasonal trends in customer retention rates, enabling them to plan
+and allocate resources accordingly.
+Furthermore, the project delves into age-based purchasing patterns, examining which age brackets make the most and least purchases. This analysis enables 
+businesses to identify specific age groups that exhibit higher or lower purchase behavior, facilitating targeted marketing and sales efforts. By understanding
+the purchasing preferences of different age groups, businesses can tailor their strategies and offer to effectively engage and retain customers.
+Finally, the project investigates the average purchase value of customers within cohorts, focusing specifically on the year 2021. This analysis helps businesses
+comprehend how customer behavior and purchase habits evolve over time within different cohorts. By identifying trends and changes in average purchase values, 
+businesses can make data-driven decisions to improve their marketing strategies, optimize customer engagement, and enhance overall customer lifetime value.
 
 
 QUESTIONS
